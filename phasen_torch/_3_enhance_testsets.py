869c1e97904a79fec8df02311ac4166fbc7635f8
IMLHF <red_wind@foxmail.com>
2020-03-17 23:19:26 +0800

    add performance measurement

--- phasen_torch/_3_enhance_testsets.py
+++ phasen_torch/_3_enhance_testsets.py
@@ -61,17 +61,41 @@ if __name__ == "__main__":
   parser.add_argument('--n_process', default=1, type=int, help="n processor")
   parser.add_argument('--ckpt', default=None, type=str, help="ckpt dir")
   parser.add_argument('--noisy_phase', default=0, type=int, help='if use noisy phase')
+  parser.add_argument('--mp', default=1, type=int, help='if measure preformance')
   args = parser.parse_args()
 
   test_processor = args.n_process
   ckpt = args.ckpt
   noisy_phase = bool(args.noisy_phase)
+  if_measure_preformance = bool(args.mp)
 
   print('n_process:', args.n_process)
   print("noisy_phase:", noisy_phase)
   print('ckpt:', args.ckpt)
+  print('measure_preformance:', if_measure_preformance)
 
   main()
+
+  if not if_measure_preformance:
+    exit(0)
+  # calculate pesq, ssnr, csig, cbak, covl
+  from .sepm import compare
+  import time
+
+  for testset_name, cleanset_name in zip(PARAM.test_noisy_sets, PARAM.test_clean_sets):
+    print("Calculate PM %s:" % testset_name, flush=True)
+    ref_dir = str(misc_utils.datasets_dir().joinpath(cleanset_name))
+    deg_dir = str(misc_utils.enhanced_testsets_save_dir(testset_name))
+    t1 = time.time()
+    res = compare(ref_dir, deg_dir)
+    t2 = time.time()
+
+    pm = np.array([x[1:] for x in res])
+    pm = np.mean(pm,axis=0)
+    print('time: %.3f' % (t2-t1))
+    print('ref=', ref_dir)
+    print('deg=', deg_dir)
+    print('csig:%6.4f cbak:%6.4f covl:%6.4f pesq:%6.4f ssnr:%6.4f'% tuple(pm))
 
   """
   run cmd:
